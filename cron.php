a39a7826a64b9a8c646493fd08f24809772a5ccc
bibr07 <46563954+bibr07@users.noreply.github.com>
2020-01-23 17:17:05 +0100

    Toevoegen bestanden

---
+++ cron.php
@@ -0,0 +1,3 @@
+C:\xampp\php\php.exe C:\xampp\htdocs\magento\bin\magento cron:run
+C:\xampp\php\php.exe C:\xampp\htdocs\magento\update\cron.php
+C:\xampp\php\php.exe C:\xampp\htdocs\magento\bin\magento setup:cron:run
